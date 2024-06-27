85afbdb1a178f5ce70da557dd41318625f4cabab
Ivliro <miro.krotky@gmail.com>
2024-06-27 10:08:26 +0200

    add handle_move method

--- src/graphics.py
+++ src/graphics.py
@@ -3,10 +3,13 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Tic-Tac-Toe")
 
+buttons = [[None for _ in range(3)] for _ in range(3)]
 for row in range(3):
     for column in range(3):
-        button = tk.Button(root, text="", width=10, height=3)
+        button = tk.Button(root, text="", width=10, height=3, \
+                    command=lambda r=row, c=column: handle_move(r,c))
         button.grid(row=row, column=column)
+        buttons[row][column] = button
 
 tk.Label(root, text="Player X's Turn").grid(row=3, columnspan=3)
 
@@ -16,11 +19,10 @@ canvas = tk.Canvas(root, width=300, height=300)
 game_board = [["" for _ in range(3)] for _ in range(3)]
 
 player_turn = "X"
-def take_move(row, column, player):
+def take_move(row, column):
     global player_turn
-    if player == "X":
+    if player_turn == "X":
         game_board[row][column] = "X"
-        
         player_turn = "O"
     else:
         game_board[row][column] = "O"
@@ -28,6 +30,22 @@ def take_move(row, column, player):
 
     button = tk.Button(root, text=game_board[row][column], width=10, height=3)
     button.grid(row=row, column=column)
+
+def handle_move(row, column):
+    if game_board[row][column] == "":
+        take_move(row, column)
+        is_x_turn = player_turn == "X"
+        buttons[row][column].config(text=game_board[row][column])
+        if check_winner():
+            tk.Label(root, text=f"Player {'O' if is_x_turn else 'X'} wins!").grid(row=3, columnspan=3)
+            # end game so that player can't click on other buttons
+            for row in range(3):
+                for column in range(3):
+                    buttons[row][column].config(state="disabled")
+        elif check_tie():
+            tk.Label(root, text="Tie!").grid(row=4, columnspan=3)
+        else:
+            tk.Label(root, text="Player " + player_turn + "'s Turn").grid(row=3, columnspan=3)
 
 def check_winner():
     # check rows
@@ -52,7 +70,4 @@ def check_tie():
                 return False
     return True
 
-take_move(0, 0, player_turn)
-take_move(0, 1, player_turn)
-take_move(0, 2, player_turn)
 root.mainloop()
